6ee20ae35c5efc8bfa3946279ee751148f0db1d9
thejoaov <jvictorsantos852@gmail.com>
2019-09-30 01:51:14 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,9 +2,9 @@ import axios from 'axios'
 
 const api = axios.create({
 	baseURL: 'https://api.github.com',
-	// headers: {
-	// 	Authorization: `token ${process.env.GITHUB_TOKEN}`,
-	// },
+	headers: {
+		Authorization: `token ${process.env.GITHUB_TOKEN}`,
+	},
 })
 
 export default api
